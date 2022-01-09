abb7f01833a5ff30c30ab4dde65af3f068c08094
Emil Alipiev <10422347+EmilAlipiev@users.noreply.github.com>
2022-01-09 01:01:29 +0100

    Demo project added

---
+++ Demo/myXFToastDemo/myXFToastDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace myXFToastDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Plugin.myXFToast.CrossmyXFToast.Current.Show("hello world", bgColorHex: "#FF0000");
+        }
+    }
+}
